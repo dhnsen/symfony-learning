e86053dcbfb6b514e6c8b943f4e29aaaa1c4ece1
Dan H <spag1024@hotmail.com>
2019-04-11 20:10:27 -0500

    created a Default controller and routed using annotations

---
+++ skeleton/src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    /**
+     * @Route()
+     */
+    public function index()
+    {
+        return new Response('My message!');
+    }
+}
+
+?>
